81791de81e9953f0448b5b5fd152d81c114a8770
Girish Kamath <girish.kamat@gmail.com>
2018-07-04 18:00:20 +0100

    updated API mock url

--- src/coinMarketCapAPI.js
+++ src/coinMarketCapAPI.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 class CoinMarketCapAPI {
+    url = "https://mockcoinmarketcap.herokuapp.com"
+
     listingsWithPrices(currency, start, limit) {
-        return axios.get(`http://localhost:9000/v2/ticker/?structure=array&start=${start}&limit=${limit}&convert=${currency}`)         
+        return axios.get(`${url}/v2/ticker/?structure=array&start=${start}&limit=${limit}&convert=${currency}`)         
     }
 }
 
